ea8b6300f6a96905f22aefb3c62aeab7e41c8f27
HaimOzer <sandromoya@icloud.com>
2024-01-02 22:18:56 +0200

    add option and arguments

--- gendiff.js
+++ gendiff.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
-const program = new Command();
+// const { Command } = require('commander')
+// const program = new Command()
+import { Command } from 'commander'
+const program = new Command()
 
 program
-  .description('Compares two configuration files and shows the differences')
-  .version('1.0.0', '-V, --version', 'Get version number')
-  .option('-h, --help', 'Get manual info');
+	.description('Compares two configuration files and shows a difference.')
+	.version('1.0.0', '-V, --version', 'output the version number')
+	.option('-h, --help', 'output usage information')
+	.option('-f, --format <type>', 'output format')
+	.arguments('[filepath1] [filepath2]')
 
-program.parse(process.argv);
+program.parse(process.argv)
 
 if (program.help) {
-  program.outputHelp();
+	program.outputHelp()
 }
+// const diff = genDiff(filepath1, filepath2)
+// console.log(diff)
+
+// if (program.help) {
+// 	console.log('Usage: gendiff [options] <filepath1> <filepath2>\n')
+// 	console.log('Compares two configuration files and shows a difference.\n')
+// 	console.log('Options:')
+// 	console.log('  -V, --version        output the version number')
+// 	console.log('  -h, --help           output usage information')
+// 	console.log('  -f, --format <type>  output format\n')
+// }
