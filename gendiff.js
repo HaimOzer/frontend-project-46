a7833ad2df77bb40f90c5ab671ad3f45b1f4b223
HaimOzer <sandromoya@icloud.com>
2024-01-03 22:19:17 +0200

    add parser

--- gendiff.js
+++ gendiff.js
@@ -3,6 +3,8 @@
 // const { Command } = require('commander')
 // const program = new Command()
 import { Command } from 'commander'
+import parseFile from './fileParser.js'
+
 const program = new Command()
 
 program
@@ -10,21 +12,20 @@ program
 	.version('1.0.0', '-V, --version', 'output the version number')
 	.option('-h, --help', 'output usage information')
 	.option('-f, --format <type>', 'output format')
-	.arguments('[filepath1] [filepath2]')
+	.arguments('<filepath1> <filepath2>')
+	.action((filepath1, filepath2) => {
+		const data1 = parseFile(filepath1)
+		const data2 = parseFile(filepath2)
+
+		console.log('Data from file 1:', data1)
+		console.log('Data from file 2:', data2)
+	})
 
 program.parse(process.argv)
 
-if (program.help) {
+if (process.argv.includes('-h') || process.argv.includes('--help')) {
 	program.outputHelp()
 }
+
 // const diff = genDiff(filepath1, filepath2)
 // console.log(diff)
-
-// if (program.help) {
-// 	console.log('Usage: gendiff [options] <filepath1> <filepath2>\n')
-// 	console.log('Compares two configuration files and shows a difference.\n')
-// 	console.log('Options:')
-// 	console.log('  -V, --version        output the version number')
-// 	console.log('  -h, --help           output usage information')
-// 	console.log('  -f, --format <type>  output format\n')
-// }
