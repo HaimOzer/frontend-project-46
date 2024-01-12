3ebba675f7d861909028906650310ef4d7599977
HaimOzer <sandromoya@icloud.com>
2024-01-12 20:31:31 +0200

    add bin directory

---
+++ bin/gendiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import { program } from 'commander'
+import genDiff from '../gendiff-report.js'
+
+program
+	.name('gendiff')
+	.description('Compares two configuration files and shows a difference.')
+	.version('0.0.1', '-v, --version', 'output the version number')
+	.arguments('<filepath1> <filepath2>')
+	.option('-f, --format <type>', 'output format', 'stylish')
+	.action((filepath1, filepath2) => {
+		const option = program.opts()
+		console.log(genDiff(filepath1, filepath2, option.format))
+	})
+
+program.parse()
