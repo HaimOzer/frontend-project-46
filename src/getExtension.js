63d488f2bd455d1ddf8a0c270610d62ad6fb3b44
HaimOzer <sandromoya@icloud.com>
2024-01-13 18:37:14 +0200

    update getExtension

---
+++ src/getExtension.js
@@ -0,0 +1,12 @@
+/**
+ * Gets the file extension from a given path.
+ *
+ * @param {string} data - The path from which to extract the file extension.
+ * @returns {string} The file extension without the leading dot.
+ */
+
+import path from 'path'
+
+const getExtension = data => path.extname(data).replace('.', '')
+
+export default getExtension
