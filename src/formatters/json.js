186ada5d65c3febbc56c8425a0e7fa465889209b
HaimOzer <sandromoya@icloud.com>
2024-01-13 18:38:02 +0200

    add json formatter

---
+++ src/formatters/json.js
@@ -0,0 +1,10 @@
+/**
+ * Converts the provided data object to a JSON-formatted string.
+ *
+ * @param {any} data - The data object to be converted to JSON.
+ * @returns {string} A JSON-formatted string representing the provided data.
+ */
+
+const jsonFormat = data => JSON.stringify(data)
+
+export default jsonFormat
