139775bbd57f3397cb98dbbc26ec3e969760fbd1
HaimOzer <sandromoya@icloud.com>
2024-01-17 22:17:07 +0200

    update gendiff test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,10 +3,9 @@ import getFixturesPath from '../src/getFixturesPath.js'
 import { fileURLToPath } from 'url'
 import getExtension from '../src/getExtension.js'
 import getContentFile from '../src/getContentFile.js'
-import parse from '../src/parsers.js'
 import yaml from 'js-yaml'
 import genDiff from '../src/getDifferents.js'
-import stylishFormat from '../src/formatters/stylish.js'
+import formatSelector from '../src/formatters/index.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -29,80 +28,6 @@ describe('getExtension function', () => {
 	})
 })
 
-describe('parse function', () => {
-	test('should parse JSON data correctly', () => {
-		const jsonData = '{"name": "John", "age": 25}'
-		const parsedData = parse(jsonData, 'json')
-		expect(parsedData).toEqual({ name: 'John', age: 25 })
-	})
-
-	test('should parse YAML data correctly', () => {
-		const yamlData = 'name: John\nage: 25'
-		const parsedData = parse(yamlData, 'yaml')
-		expect(parsedData).toEqual({ name: 'John', age: 25 })
-	})
-
-	test('should parse YAML data correctly when format is "yml"', () => {
-		const yamlData = 'name: John\nage: 25'
-		const parsedData = parse(yamlData, 'yml')
-		expect(parsedData).toEqual({ name: 'John', age: 25 })
-	})
-
-	test('should throw an error for unknown format', () => {
-		const invalidFormat = 'csv'
-		expect(() => parse('', invalidFormat)).toThrowError(
-			`Unknown type! Type ${invalidFormat} is not supported!`
-		)
-	})
-})
-
-describe('stylishFormat function', () => {
-	test('should format unchanged node correctly', () => {
-		const tree = [{ type: 'unchanged', key: 'name', value: 'John' }]
-		const result = stylishFormat(tree)
-		expect(result).toEqual('{\n    name: John\n}')
-	})
-
-	test('should format added node correctly', () => {
-		const tree = [{ type: 'added', key: 'name', value: 'John' }]
-		const result = stylishFormat(tree)
-		expect(result).toEqual('{\n  + name: John\n}')
-	})
-
-	test('should format removed node correctly', () => {
-		const tree = [{ type: 'removed', key: 'name', value: 'John' }]
-		const result = stylishFormat(tree)
-		expect(result).toEqual('{\n  - name: John\n}')
-	})
-
-	test('should format updated node correctly', () => {
-		const tree = [
-			{ type: 'updated', key: 'name', value1: 'John', value2: 'Doe' },
-		]
-		const result = stylishFormat(tree)
-		expect(result).toEqual('{\n  - name: John\n  + name: Doe\n}')
-	})
-
-	test('should format nested node correctly', () => {
-		const tree = [
-			{
-				type: 'nested',
-				key: 'person',
-				children: [{ type: 'unchanged', key: 'name', value: 'John' }],
-			},
-		]
-		const result = stylishFormat(tree)
-		expect(result).toEqual('{\n    person: {\n        name: John\n    }\n}')
-	})
-
-	test('should throw an error for unknown node type', () => {
-		const tree = [{ type: 'unknown', key: 'name', value: 'John' }]
-		expect(() => stylishFormat(tree)).toThrowError(
-			'Unknown type! unknown is wrong!'
-		)
-	})
-})
-
 describe('genDiff function', () => {
 	const cases = ['json', 'yaml', 'yml']
 	const expectedStylish = getContentFile('expectedStylishFormat.txt')
@@ -122,5 +47,12 @@ describe('genDiff function', () => {
 		expect(currentStylishFormat).toEqual(expectedStylish)
 		expect(currentPlainFormat).toEqual(expectedPlain)
 		expect(currentJSONFormat).toEqual(expectedJSON)
+	})
+
+	test('should throw an error for unknown format type', () => {
+		const data = getFixturesPath('file1.json')
+		expect(() => formatSelector(data, 'stringify')).toThrow(
+			'Unknown format type'
+		)
 	})
 })
