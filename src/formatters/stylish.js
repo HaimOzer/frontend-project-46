d085d049af8ff71bc438482108c21f7fef6e90fb
HaimOzer <sandromoya@icloud.com>
2024-01-19 16:59:25 +0200

    refactoring stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 /**
  * Converts a difference tree to a stylishly formatted string.
  *
@@ -7,28 +9,31 @@
  * @param {string} [indent=' '] - The string to use for indentation.
  * @returns {string} A stylishly formatted string representing the differences in the tree.
  */
-
-import _ from 'lodash'
-
-const stringify = (value, depth = 1, indentCount = 4, indent = ' ') => {
-	if (!_.isObject(value)) {
-		return value
-	}
-	const indentSize = depth * indentCount
-	const currentIndent = indent.repeat(indentSize)
-	const bracketsIndent = indent.repeat(indentSize - indentCount)
-	const result = Object.keys(value).map(
-		key => `${currentIndent}${key}: ${stringify(value[key], depth + 1)}`
-	)
-	return ['{', ...result, `${bracketsIndent}}`].join('\n')
-}
-
 function stylishFormat(tree, depth = 1, indentCount = 4, indent = ' ') {
-	const indentSize = depth * indentCount
-	const currentIndent = indent.repeat(indentSize)
-	const changedLineIndent = indent.repeat(indentSize - 2)
-	const bracketsIndent = indent.repeat(indentSize - indentCount)
-	const result = tree.map(node => {
+	const getIndent = size => indent.repeat(size)
+
+	const stringify = (value, currentDepth) => {
+		if (!_.isObject(value)) {
+			return value
+		}
+
+		const result = Object.entries(value).map(
+			([key, val]) =>
+				`${getIndent(currentDepth * indentCount)}${key}: ${stringify(
+					val,
+					currentDepth + 1
+				)}`
+		)
+
+		return `{\n${result.join('\n')}\n${getIndent(
+			(currentDepth - 1) * indentCount
+		)}}`
+	}
+
+	const formatNode = node => {
+		const currentIndent = getIndent(depth * indentCount)
+		const changedLineIndent = getIndent(depth * indentCount - 2)
+
 		switch (node.type) {
 			case 'unchanged':
 				return `${currentIndent}${node.key}: ${stringify(
@@ -64,7 +69,68 @@ function stylishFormat(tree, depth = 1, indentCount = 4, indent = ' ') {
 			default:
 				throw new Error(`Unknown type! ${node.type} is wrong!`)
 		}
-	})
-	return ['{', ...result, `${bracketsIndent}}`].join('\n')
+	}
+
+	return `{\n${tree.map(formatNode).join('\n')}\n}`
 }
+
 export default stylishFormat
+
+// const stringify = (value, depth = 1, indentCount = 4, indent = ' ') => {
+// 	if (!_.isObject(value)) {
+// 		return value
+// 	}
+// 	const indentSize = depth * indentCount
+// 	const currentIndent = indent.repeat(indentSize)
+// 	const bracketsIndent = indent.repeat(indentSize - indentCount)
+// 	const result = Object.keys(value).map(
+// 		key => `${currentIndent}${key}: ${stringify(value[key], depth + 1)}`
+// 	)
+// 	return ['{', ...result, `${bracketsIndent}}`].join('\n')
+// }
+
+// function stylishFormat(tree, depth = 1, indentCount = 4, indent = ' ') {
+// 	const indentSize = depth * indentCount
+// 	const currentIndent = indent.repeat(indentSize)
+// 	const changedLineIndent = indent.repeat(indentSize - 2)
+// 	const bracketsIndent = indent.repeat(indentSize - indentCount)
+// 	const result = tree.map(node => {
+// 		switch (node.type) {
+// 			case 'unchanged':
+// 				return `${currentIndent}${node.key}: ${stringify(
+// 					node.value,
+// 					depth + 1
+// 				)}`
+// 			case 'added':
+// 				return `${changedLineIndent}+ ${node.key}: ${stringify(
+// 					node.value,
+// 					depth + 1
+// 				)}`
+// 			case 'removed':
+// 				return `${changedLineIndent}- ${node.key}: ${stringify(
+// 					node.value,
+// 					depth + 1
+// 				)}`
+// 			case 'updated':
+// 				return [
+// 					`${changedLineIndent}- ${node.key}: ${stringify(
+// 						node.value1,
+// 						depth + 1
+// 					)}`,
+// 					`${changedLineIndent}+ ${node.key}: ${stringify(
+// 						node.value2,
+// 						depth + 1
+// 					)}`,
+// 				].join('\n')
+// 			case 'nested':
+// 				return `${currentIndent}${node.key}: ${stylishFormat(
+// 					node.children,
+// 					depth + 1
+// 				)}`
+// 			default:
+// 				throw new Error(`Unknown type! ${node.type} is wrong!`)
+// 		}
+// 	})
+// 	return ['{', ...result, `${bracketsIndent}}`].join('\n')
+// }
+// export default stylishFormat
